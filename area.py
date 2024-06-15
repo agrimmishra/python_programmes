1ea5ac9adf1ed5231d68468c5f35e4faf0f5fc47
agrim_mishra <agrim12mishra@gmail.com>
2024-06-15 10:16:48 +0530

    commit

--- area.py
+++ area.py
@@ -1,20 +1,20 @@
 import math
-define pi = 
+
 shape = input("enter the shapes name :")
 if shape == "rectangle" or shape == "square":
-    height = int(input("enter the height :"))
-    width = int(input("enter the width :"))
+    height = float(input("enter the height :"))
+    width = float(input("enter the width :"))
     area = height*width
     print(area)
 elif shape == "triangle":
-    height = int(input("enter the height :"))
-    base = int(input("enter the base:"))
+    height = float(input("enter the height :"))
+    base = float(input("enter the base:"))
     area = 0.5* (height*base)
-    print(area)
+    prfloat(area)
 elif shape == "circle":
-    radius = int(input("enter the radius :"))
+    radius = float(input("enter the radius :"))
     area = math.pi * (radius**2)
-    print("%.2f" %area)
+    print("%.3f" %area)
 elif shape == "":
     print("fill the shape name feild")
 else:
