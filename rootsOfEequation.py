dd2486845552b86dfd7106c2ee094ece36ba3d9e
agrim_mishra <agrim12mishra@gmail.com>
2024-06-16 07:14:55 +0530

    commit

--- rootsOfEequation.py
+++ rootsOfEequation.py
@@ -1,14 +1,14 @@
-a = int(input("enter the value of coefficient of x^2 :"))
+a = int(input("enter the value of coefficient of x^2 :"))                                       #  taking in the co-efficient of the eqution      
 b = int(input("enter the value of coefficient of x :"))
 c = int(input("enter the value of constant :"))
-checker = (b**2)-(4*(a*c))
+checker = (b**2)-(4*(a*c))                                                                                #  checking the behavoir of the roots of equation
 print(checker)
-if checker >0:
+if checker >0:                                                                                                   # conditon for unequal roots 
     print("the equation has real unequal roots :")
     x1 = ( (-1*b) - (checker**(1/2)))/(2*a)
     x2 = ( (-1*b) + (checker**(1/2)))/(2*a)
     print(x1,x2)
-elif checker ==0 :
+elif checker ==0 :                                                                                          # condition for equal roots
      print("the equation has real equal roots :")
      x1 = ( (-1*b) - (checker**(1/2)))/(2*a)
      print(x1)
