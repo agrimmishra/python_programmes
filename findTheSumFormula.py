cadad3a646188b7d6cce13ca13019095715bde76
agrim_mishra <agrim12mishra@gmail.com>
2024-06-23 09:32:52 +0530

    commits

--- findTheSumFormula.py
+++ findTheSumFormula.py
@@ -1,4 +1,4 @@
 terms = int(input("enter the no of terms :"))
 sum = 0
-sum =int(((terms)*(terms+1)*(2*terms +1 ))/6 )
+sum =int((terms)*(terms+1)/2 )
 print(sum)
---
+++ userNameFinder.py
@@ -0,0 +1,5 @@
+
+
+email = input("enter email:")
+atRate= email.find("@")
+print("username =",email[0:atRate:1])
