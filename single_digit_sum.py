4699f91d87f894a1836b5e88c3646913daabeaba
agrim_mishra <agrim12mishra@gmail.com>
2024-07-31 19:32:09 +0530

    commit docstring

--- single_digit_sum.py
+++ single_digit_sum.py
@@ -1,19 +1,35 @@
 def sumNum(givenNum):
+    """
+    Computes the digital root of a given number.
+
+    The digital root is the iterative process of summing the digits of a number until a single digit is obtained.
+
+    Parameters:
+    givenNum (int): The number whose digital root is to be calculated.
+
+    Returns:
+    int: The digital root of the given number.
+    """
+    # Initialize a variable to store the sum of digits
     number = givenNum
-    sum=0
-    while number>0:
-        rem = number%10
-        sum+=rem
-        number//=10
-        newNum= sum
-  
-    if(newNum>=10):
-       sumNum(newNum)
+    sum = 0
+
+    # Calculate the sum of digits of the number
+    while number > 0:
+        rem = number % 10  # Get the last digit
+        sum += rem         # Add it to the sum
+        number //= 10      # Remove the last digit
+
+    # If the sum is a single digit, return it
+    if sum < 10:
+        return sum
     else:
-       print(newNum)
-    
-    
-num = int(input("enter number:"))
-sumNum(num)
-          
-          
+        # Recursively call sumNum to reduce the sum to a single digit
+        return sumNum(sum)
+
+# Input number from the user
+num = int(input("Enter number: "))
+
+# Calculate and print the digital root of the input number
+result = sumNum(num)
+print("The digital root is:", result)
