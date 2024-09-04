1aca1a1e10fd9e8040dd315932b0416933df294f
agrim_mishra <agrim12mishra@gmail.com>
2024-09-04 21:35:18 +0530

    linear search implemented

--- dsa python/linearSearch.py
+++ dsa python/linearSearch.py
@@ -1,1 +1,13 @@
-def
+def linearSearch(list,target):
+    for item in list:
+        if item == target:
+            return 1
+        else:
+            return 0;
+        
+list = [22,33,44,67,88,00]
+result = linearSearch(list,9)
+if result == 1:
+    print("element found");
+else:
+    print("element not found")
