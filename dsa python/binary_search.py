7446e70aea434b21de5936b496f503225778f3ec
agrim_mishra <agrim12mishra@gmail.com>
2024-09-11 08:26:59 +0530

    commit

---
+++ dsa python/binary_search.py
@@ -0,0 +1,18 @@
+list = [1,3,5,7,99,198]
+
+def Binary(list,target):
+    high = len(list)-1
+    low = 0
+    while low<= high:
+        mid = (high + low) //2
+        if target == list[mid]:
+            return mid
+        elif(target>list[mid]):
+            low = mid + 1
+        elif target<list[mid]:
+            high = mid - 1
+    return -1
+    
+index = Binary(list,99)
+if index >-1:
+    print(f"the element is present on {index} index ")
